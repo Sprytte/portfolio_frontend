b1d3670d93717c86c69719b3ba39d84d4569e656
Émilie Parent <lolabff@hotmail.com>
2024-02-13 02:03:42 -0500

    api change

--- src/Components/Constants.js
+++ src/Components/Constants.js
@@ -1,2 +1,2 @@
-// export const api = "https://canadawidecarparts.nn.r.appspot.com/api/v1"
-export const api = "http://localhost:5000/api/v1"
+export const api = "https://canadawidecarparts.nn.r.appspot.com/api/v1"
+// export const api = "http://localhost:5000/api/v1"
